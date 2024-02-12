9c0d926ab9b4ef290c95a72ada299012fde9c7d7
Gab <59850744+G40b@users.noreply.github.com>
2024-02-12 14:32:57 -0300

    Initial commit

---
+++ src/livraria/bean/AutorBean.java
@@ -0,0 +1,22 @@
+package livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+import livraria.dao.DAO;
+import livraria.model.Autor;
+
+@ManagedBean
+public class AutorBean {
+
+	private Autor autor = new Autor();
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando autor " + this.autor.getNome());
+
+		new DAO<Autor>(Autor.class).adiciona(this.autor);
+	}
+}
