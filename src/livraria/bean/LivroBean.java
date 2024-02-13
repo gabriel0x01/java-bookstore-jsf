a77d7ecff6a155c9e78d5f269b082ba1cfa786c1
Gab <59850744+G40b@users.noreply.github.com>
2024-02-12 21:12:19 -0300

    Completes system and handles scopes

--- src/livraria/bean/LivroBean.java
+++ src/livraria/bean/LivroBean.java
@@ -1,17 +1,45 @@
 package livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import livraria.dao.DAO;
+import livraria.model.Autor;
 import livraria.model.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
+	private Integer autorId;
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
 
 	public Livro getLivro() {
 		return livro;
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+
+	
+	public void gravarAutor() { 
+		Autor autorSelecionado = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autorSelecionado);
 	}
 
 	public void gravar() {
@@ -22,6 +50,13 @@ public class LivroBean {
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		
+	    this.livro = new Livro();
+
 	}
+	
+	public List<Livro> getLivros() {
+		  return new DAO<Livro>(Livro.class).listaTodos();
+		}
 
 }
