ea370ec8d4e6aa00386d682e4cdd1516cf09c1e6
Gab <59850744+G40b@users.noreply.github.com>
2024-02-13 20:22:34 -0300

    Handles hours, currency and timezone

--- src/livraria/dao/PopulaBanco.java
+++ src/livraria/dao/PopulaBanco.java
@@ -28,24 +28,24 @@ public class PopulaBanco {
 		em.persist(coelho);
 
 		Livro casmurro = geraLivro("978-8-52-504464-8", "Dom Casmurro",
-				"10/01/1899", 24.90, assis);
+				"10/01/1899 12:00", 24.90, assis);
 		Livro memorias = geraLivro("978-8-50-815415-9",
-				"Memorias Postumas de Bras Cubas", "01/01/1881", 19.90, assis);
+				"Memorias Postumas de Bras Cubas", "01/01/1881 12:00", 19.90, assis);
 		Livro quincas = geraLivro("978-8-50-804084-1", "Quincas Borba",
-				"01/01/1891", 16.90, assis);
+				"01/01/1891 12:00", 16.90, assis);
 
 		em.persist(casmurro);
 		em.persist(memorias);
 		em.persist(quincas);
 
 		Livro alquemista = geraLivro("978-8-57-542758-3", "O Alquimista",
-				"01/01/1988", 19.90, coelho);
-		Livro brida = geraLivro("978-8-50-567258-7", "Brida", "01/01/1990",
+				"01/01/1988 10:00", 19.90, coelho);
+		Livro brida = geraLivro("978-8-50-567258-7", "Brida", "01/01/1990 10:00",
 				12.90, coelho);
 		Livro valkirias = geraLivro("978-8-52-812458-8", "As Valkirias",
-				"01/01/1992", 29.90, coelho);
+				"01/01/1992 10:00", 29.90, coelho);
 		Livro maao = geraLivro("978-8-51-892238-9", "O Diario de um Mago",
-				"01/01/1987", 9.90, coelho);
+				"01/01/1987 10:00", 9.90, coelho);
 
 		em.persist(alquemista);
 		em.persist(brida);
@@ -53,9 +53,9 @@ public class PopulaBanco {
 		em.persist(maao);
 
 		Livro capitaes = geraLivro("978-8-50-831169-1", "Capitaes da Areia",
-				"01/01/1937", 6.90, amado);
+				"01/01/1937 08:00", 6.90, amado);
 		Livro flor = geraLivro("978-8-53-592569-9",
-				"Dona Flor e Seus Dois Maridos", "01/01/1966", 18.90, amado);
+				"Dona Flor e Seus Dois Maridos", "01/01/1966 08:00", 18.90, amado);
 
 		em.persist(capitaes);
 		em.persist(flor);
@@ -85,7 +85,7 @@ public class PopulaBanco {
 	@SuppressWarnings("unused")
 	private static Calendar parseData(String data) {
 		try {
-			Date date = new SimpleDateFormat("dd/MM/yyyy").parse(data);
+			Date date = new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(data);
 			Calendar calendar = Calendar.getInstance();
 			calendar.setTime(date);
 			return calendar;
